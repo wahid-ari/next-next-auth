5da80ec34f674e1ad3a8055a57b8c9be406d22de
wahid-ari <wahiid.arii@Gmail.com>
2022-09-25 17:00:10 +0700

    update

--- pages/signout.js
+++ pages/signout.js
@@ -1,19 +1,14 @@
 import { signOut } from 'next-auth/react'
 import { useEffect } from 'react';
-import Router from "next/router";
 
 export default function Signout() {
   useEffect(() => {
     localStorage.removeItem("user-id");
     localStorage.removeItem("user-name");
     localStorage.removeItem("user-email");
-    Router.replace("/signin");
   }, []);
 
   signOut({ callbackUrl: '/signin' });
 
-  return (
-    <>
-    </>
-  )
+  return ("")
 }
